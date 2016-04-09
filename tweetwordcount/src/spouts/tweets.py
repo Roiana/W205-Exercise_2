91716c9788a63b104f6fce98a90ffc05af671bbd
Roiana <roiana.reid@gmail.com>
2016-04-09 08:37:49 -0400

    Update tweets.py

--- tweetwordcount/src/spouts/tweets.py
+++ tweetwordcount/src/spouts/tweets.py
@@ -46,3 +46,44 @@ class Tweets(Spout):
 
     def initialize(self, stormconf, context):
         self._queue = Queue.Queue(maxsize = 100)
+        
+        consumer_key = auth_get("consumer_key")
+        consumer_secret = auth_get("consumer_secret")
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+        if auth_get("access_token") and auth_get("access_token_secret"):
+            access_token = auth_get("access_token")
+            access_token_secret = auth_get("access_token_secret")
+            auth.set_access_token(access_token, access_token_secret)
+
+        self._tweepy_api = tweepy.API(auth)
+
+        # Create the listener for twitter stream
+        listener = TweetStreamListener(self)
+
+        # Create the stream and listen for english tweets
+        stream = tweepy.Stream(auth, listener, timeout=None)
+        stream.filter(languages=["en"], track=["a", "the", "i", "you", "u"], async=True)
+
+    def queue(self):
+        return self._queue
+
+    def tweepy_api(self):
+        return self._tweepy_api
+
+    def next_tuple(self):
+        try:
+            tweet = self.queue().get(timeout = 0.1)
+            if tweet:
+                self.queue().task_done()
+                self.emit([tweet])
+
+        except Queue.Empty:
+            self.log("Empty queue exception ")
+            time.sleep(0.1)
+
+    def ack(self, tup_id):
+        pass  # if a tuple is processed properly, do nothing
+
+    def fail(self, tup_id):
+        pass  # if a tuple fails to process, do nothing
