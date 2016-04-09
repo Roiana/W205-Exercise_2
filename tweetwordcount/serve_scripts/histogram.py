efec21d505713177efab78693e400e4d76bada79
Roiana <roiana.reid@gmail.com>
2016-04-09 10:09:44 -0400

    Create histogram.py

---
+++ tweetwordcount/serve_scripts/histogram.py
@@ -0,0 +1,20 @@
+import psycopg2
+import operator
+import sys
+
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+
+cur = conn.cursor()
+cur.execute("SELECT word, count from tweetswordcount")
+records = cur.fetchall()
+
+sortrec = sorted(records, key=operator.itemgetter(1))
+
+print sys.argv[1]
+
+fi = sys.argv[1].split(",")[0]
+si = sys.argv[1].split(",")[1]
+
+for rec in sortrec:
+    if (rec[1] >= int(fi) and rec[1] <= int(si)):
+        print rec[0], ":", rec[1]
